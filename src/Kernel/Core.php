8aa69576686c6c0ae999947cb24849b02c6c0278
tzepart <tzepart@gmail.com>
2018-10-26 15:36:36 +0300

    add_500_error_page

--- src/Kernel/Core.php
+++ src/Kernel/Core.php
@@ -33,9 +33,13 @@ final class Core
 
     public function initResponse()
     {
-        $router = new Router();
-        //Call a callback with array of params
-        call_user_func_array([$router->getCurrentController(), $router->getCurrentMethod()], $router->getParams());
+        try{
+            $router = new Router();
+            //Call a callback with array of params
+            call_user_func_array([$router->getCurrentController(), $router->getCurrentMethod()], $router->getParams());
+        }catch (\Exception $exception){
+            require_once APP_ROOT."public/500.html";
+        }
     }
 
     private function __clone(){}

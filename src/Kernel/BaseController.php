0b512dfb90266abf421e62bb3a0e9e56d9122b7e
tzepart <tzepart@gmail.com>
2018-10-26 11:33:13 +0300

    add_BaseController_class

---
+++ src/Kernel/BaseController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Kernel;
+
+
+/**
+ * Base Controller
+ * Loads views
+ * Class Controller
+ */
+abstract class BaseController
+{
+
+    /**
+     * Load view
+     * @param $view
+     * @param array $data
+     */
+    public function view($view, $data = [])
+    {
+      // Check for view file
+      if(file_exists(APP_SRC_ROOT.'View/' . $view . '.php')) {
+         require_once (APP_SRC_ROOT.'View/' . $view . '.php');
+      } else{
+         /// View does not exists
+         die ('View does not exists');
+      }
+    }
+}

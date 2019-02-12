5969a32c27c35f14dcc5426fed03c283fe326559
tzepart <tzepart@gmail.com>
2019-02-12 23:11:46 +0300

    w_add_ExceptionCode_and_HttpNotFoundException

---
+++ src/Exception/ExceptionCode.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: artem
+ * Date: 2019-02-12
+ * Time: 23:08
+ */
+
+namespace App\Exception;
+
+
+/**
+ * Class ExceptionCode
+ * @package App\Exception
+ */
+class ExceptionCode
+{
+    const NOT_FOUND_CODE = "10001";
+}
